d4477acf64d211a71a66213799ccb7ebe54983d0
Haseeb <haseebasif5@gmail.com>
2025-01-17 15:06:09 +0500

    Mongo/logger/exception setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Haseeb Asif",
+    author_email="haseebasif5@gmail.com",
+    packages=find_packages()
+)
